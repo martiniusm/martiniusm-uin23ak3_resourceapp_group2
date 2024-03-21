9445c49aaa0c1bceb11e15296ca2c309dffd465f
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-03-21 20:25:37 +0100

    .

--- ressursarkiv/src/Components/Layout.jsx
+++ ressursarkiv/src/Components/Layout.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import Nav from './Nav'
 import { useParams } from 'react-router-dom'
 
-export default function Layout({resources, children, category, setCategory}) {
+export default function Layout({resources, children, category}) {
     
     return (
             <>
-            <Nav resources={resources} category={category} />
+            <Nav resources={resources} category={category}/>
             {children}
             </>
     )
