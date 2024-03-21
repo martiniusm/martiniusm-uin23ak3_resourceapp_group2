f7f7baed124620b2e009deb1200fec585b4cca51
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-03-21 17:02:13 +0100

    routing scss fix

--- ressursarkiv/src/Components/Nav.jsx
+++ ressursarkiv/src/Components/Nav.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function Nav({ handleCategorySelect, categories }) {
-    const {id} = useParams()
+    const {catId} = useParams()
 
     const [active, setActive] = useState(null);
 
