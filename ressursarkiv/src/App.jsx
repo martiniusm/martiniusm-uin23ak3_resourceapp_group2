eaca47120a3f416af58535aa8cbe12faa411d5ae
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-03-21 16:40:11 +0100

    Routing-test

--- ressursarkiv/src/App.jsx
+++ ressursarkiv/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Layout from './Components/Layout'
 import { Route, Routes } from 'react-router-dom'
-import Category from './Components/Category'
 export default function App() {
     return (
         <>
             <Layout>
                 <Routes>
-                    <Route path='/' element={<Category />} />
+                    <Route path='/:CatId' element={<Layout />} />
                 </Routes>
             </Layout>
         </>
--- ressursarkiv/src/Components/Nav.jsx
+++ ressursarkiv/src/Components/Nav.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Nav({ handleCategorySelect, categories }) {
+    const {id} = useParams()
+
     const [active, setActive] = useState(null);
 
     const handleActive = (category) => {
@@ -12,7 +16,9 @@ export default function Nav({ handleCategorySelect, categories }) {
         <ul className='category'>
             {categories.map((category, i) => (
                 <li key={i} onClick={() => { handleCategorySelect(category); handleActive(category); }} className={active === category ? "active" : null}>
-                    <h2>{category}</h2>
+                    <Link to={"/"+category.replaceAll(" ", "-")} className={active === category? "active": null}>
+                        <h2>{category}</h2>
+                    </Link>
                 </li>
             ))}
         </ul>
